98301fb023640d05d5875b7f798d49344381e839
miguelborda <miguelangelbg20@gmail.com>
2017-04-14 15:22:06 -0430

    borrar duplicados

--- js/extension.js
+++ js/extension.js
@@ -2,17 +2,29 @@ Array.prototype.borrar = function(element) {
 	for (var i = 0; i < this.length; i++){
 		if (this[i] === element )
 		{
-			this.splice(i,1);
+			this.splice(i,1);			
 		}
 	}
 };
-
 Array.prototype.buscar = function(element){
+	var cont = false;
 	for (var j = 0; j < this.length; j++){
 		if (this[j] === element )
 		{
 			return ("encontrado: "+ element + " en la posicion: " + (j+1) + " del Array");
+			cont === true;
 		}
 	}
-
+	if (cont === false){
+		return ("No se encuentra el elemento: " + element + " en el Array ");
+	}
+}
+Array.prototype.borrarduplicados = function(){
+	for (var x = 0; x < this.length; x++){
+		for (var y = x+1; y < this.length; y++){
+			if(this[x] === this[y]){
+				this.splice(y);	
+			}
+		}		
+	}
 }
